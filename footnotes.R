37487023386ca5f8dda1a3c0e977fe64a6da63f2
edavidaja <enyinnaya.aja@gmail.com>
2018-09-30 14:54:07 -0400

    minimally functioning sidebar and footnote extraction

--- footnotes.R
+++ footnotes.R
@@ -1,48 +1,30 @@
-library(pdftools)
-library(jsonlite)
-library(purrr)
 library(stringr)
-library(tibble)
 library(dplyr)
+library(purrr)
 
-read_pdf <- function(file) {
-  # extract table of contents and report text 
-  
-  toc <- pdf_toc(file) %>%
-    unlist(., use.names = FALSE) %>%
-    .[1:max(str_which(., "Appendix"))]
-
-  text <- pdf_text(file) %>% 
-    str_split(., pattern = "\r\n")
-  
-  list(
-    toc = toc,
-    text = text
-  )
-}
-
-infile <- read_pdf("pdfs/693817.pdf")
-
+# footnotes appear at the bottom of a page as single number
+# that starts and ends the line
 extract_footnotes <- function(page) {
-  # footnotes appear at the bottom of a page as single number
-  # that starts and ends the line
-  # detect the indices of these footnotes, 
+ 
   if (any(str_detect(page, "^[ 0-9]+$"))) {
     footnote_start <- c(
         str_which(page, "^[ 0-9]+$"),
         length(page) - 1
       )
+    
     footnote_length <- c(diff(footnote_start) - 1, NA)
+    
     footnote_position <- 
       data_frame(footnote_start, footnote_length) %>% 
-      mutate(footnote_end = footnote_start + footnote_length) %>%
-      filter(!is.na(footnote_length))
+        mutate(footnote_end = footnote_start + footnote_length) %>%
+        filter(!is.na(footnote_length))
     
     footnote_text <- footnote_position %>%   
       map2_chr(
         .x = .$footnote_start, .y = .$footnote_end,
         .f = ~str_c(page[(.x+1):.y], collapse = " ")
       )
+    
     footnote_index <- footnote_position$footnote_start %>% 
       map_chr(~page[[.x]])
     
@@ -52,6 +34,3 @@ extract_footnotes <- function(page) {
       ) %>% map(str_squish)
     }
   }
-
-out <- map(infile$text, extract_footnotes)
-out
