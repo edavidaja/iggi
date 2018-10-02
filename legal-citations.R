0433bcc86948bdd530e4aed5e0c53539ceffa232
edavidaja <enyinnaya.aja@gmail.com>
2018-10-02 00:27:28 -0400

    first cut at extracting legal citations with node citations

---
+++ legal-citations.R
@@ -0,0 +1,26 @@
+library(httr)
+# accessor functions for grabbing the citation
+citation1 <- function(x) x$results$citations
+citation2 <- function(x) x$citation
+
+# predicate function to reject "reporter" type citations
+irrelevant <- function(x) x$type == "reporter"
+
+get_legal_citations <- function(footnotes) {
+  footnotes$text %>%
+    map(
+      ~GET(
+        url = "http://localhost:3030/citation/find",
+        query = list(text = .x)
+      )
+    ) %>% 
+    map(~content(.x, "parsed")) %>% 
+    map(~pluck(.x, citation1)) %>%
+    compact() %>% 
+    flatten() %>% 
+    discard(.p = irrelevant) %>% 
+    map(~pluck(.x, citation2)) %>%
+    compact() %>%
+    flatten_chr() %>% 
+    unique()
+}
--- parse_pdf.R
+++ parse_pdf.R
@@ -9,32 +9,36 @@ source("footnotes.R")
 source("sidebar_text.R")
 source("agency_comments.R")
 source("get_gao_citations.R")
+source("legal-citations.R")
 
 parse_pdf <- function(file) {
-  # extract table of contents and report text 
-  
+  # extract table of contents and report text
+
   toc <- pdf_toc(file) %>%
     unlist(., use.names = FALSE)
-  
-  text <- pdf_text(file) %>% 
+
+  text <- pdf_text(file) %>%
     str_split(., pattern = "\r\n")
-  
-  footnotes <- map(text, extract_footnotes) %>% 
+
+  footnotes <- map(text, extract_footnotes) %>%
     bind_rows()
-  
+
   sidebar <- map_chr(text, extract_sidebar_text)
-  
+
   gao_citations <- get_gao_citations(text)
-  
+
   agency_comments <- get_comments(file, text)
-  
+
+  legal_citations <- get_legal_citations(footnotes)
+
   list(
-    toc = toc,
-    text = text,
-    footnotes = footnotes,
-    sidebar = sidebar,
-    gao_citations = gao_citations,
-    agency_comments = agency_comments
+    toc             = toc,
+    text            = text,
+    footnotes       = footnotes,
+    sidebar         = sidebar,
+    gao_citations   = gao_citations,
+    agency_comments = agency_comments,
+    legal_citations = legal_citations
   )
 
 }
